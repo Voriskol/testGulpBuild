e466a08eeb2e0354cf3d97704b26a6fafc03495f
Kirill <pesterevkv2003@gmail.com>
2024-05-13 22:54:42 +0300

    ggg

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const cleanCSS = require("gulp-clean-css");
 const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
 const concat = require("gulp-concat");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("gulp-autoprefixer");
+const imagemin = require("gulp-imagemin");
 
 const paths = {
   styles: {
@@ -16,6 +19,10 @@ const paths = {
     src: "src/scripts/**/*.js",
     dest: "dist/js/",
   },
+  images: {
+    src: "src/img/*",
+    dest: "dist/img",
+  },
 };
 
 function clean() {
@@ -25,14 +32,25 @@ function clean() {
 function styles() {
   return gulp
     .src(paths.styles.src)
+    .pipe(sourcemaps.init())
     .pipe(less())
-    .pipe(cleanCSS())
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(
+      cleanCSS({
+        level: 2,
+      })
+    )
     .pipe(
       rename({
         basename: "main",
         suffix: ".min",
       })
     )
+    .pipe(sourcemaps.write("."))
     .pipe(gulp.dest(paths.styles.dest));
 }
 
@@ -46,15 +64,33 @@ function scripts() {
     .src(paths.scripts.src, {
       sourcemaps: true,
     })
-    .pipe(babel())
+    .pipe(sourcemaps.init())
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+      })
+    )
     .pipe(uglify())
     .pipe(concat("main.min.js"))
+    .pipe(sourcemaps.write("."))
     .pipe(gulp.dest(paths.scripts.dest));
 }
 
-const build = gulp.series(clean, gulp.parallel(styles, scripts), watch);
+function img() {
+  return gulp
+    .src(paths.images.src)
+    .pipe(
+      imagemin({
+        progressive: true,
+      })
+    )
+    .pipe(gulp.dest(paths.images.dest));
+}
+
+const build = gulp.series(clean, gulp.parallel(styles, scripts, img), watch);
 
 exports.clean = clean;
+exports.img = img;
 exports.styles = styles;
 exports.watch = watch;
 exports.build = build;
